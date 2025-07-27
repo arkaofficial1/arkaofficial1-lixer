019dbdb2253a9afb4ef2443a58060c1c412735fe
mohammad shadman <mmdshadman82@gmail.com>
2025-07-27 02:05:54 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/i18n.ts
+++ src/i18n.ts
@@ -7,6 +7,7 @@ export default getRequestConfig(async ({locale}) => {
   if (!locales.includes(locale as any)) notFound();
  
   return {
+    locale,
     messages: {
       ...(await import(`../messages/${locale}.json`)).default,
       ...(await import(`../messages/admin/${locale}.json`)).default
